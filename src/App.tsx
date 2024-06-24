109b4bf73d1982430130a6f123f9ce4e8e32861b
Daniel Leon <dalodev@proton.me>
2024-06-24 14:42:01 -0500

    Added loader, error and initial screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
+import { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+
+const initialState = {
+  questions: [],
+  status: "loading", //* 'loading', 'error', 'ready', 'active', 'finished'
+};
+
+const reducer = function (state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unknown");
+  }
+};
 
 function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const questionsAmount = questions.length;
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch(() => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen questionsLength={questionsAmount} />
+        )}
       </Main>
     </div>
   );
